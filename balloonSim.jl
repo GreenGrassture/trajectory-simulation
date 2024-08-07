ba5fb95594ff66ba7858a1039afadda237bff467
GreenGrassture <juliustereck@gmail.com>
2024-08-07 09:59:17 -0700

    Added full 3d simulation Fixed drag coefficients Added trajectory viewer

--- balloonSim.jl
+++ balloonSim.jl
@@ -1,8 +1,19 @@
 # An attempt at modeling the volume of a balloon and related variables (pressure, buoyant force etc)
 
+using LinearAlgebra
+using Statistics
 using DifferentialEquations
+#using ModelingToolkit
 using ISAData
-using Plots
+#using Plots
+using Geodesy
+
+using Parameters
+using ComponentArrays
+
+using DataFrames
+using CSV
+
 
 
 
@@ -24,9 +35,12 @@ end
 
 # These functions will likely get more complicated when I integrate information from weather data, but for now they just use ISA data
 function getAirParams(u, p, t)
-    z, _ = u
-    return ISAdata(z)
+    # returns ρ, P, T, μ
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    return ISAdata(rz) 
 end
+
+
 
 function getAirDensity(u, p, t)
     return getAirParams(u, p, t)[1]
@@ -55,7 +69,7 @@ function getBalloonVolume(u, p, t)
     # 1) The internal temperature is uniform, and the same as the external temperature
     # 2) The pressure is the same as the external pressure
     # 3) The lift gas follows the ideal gas law
-    z, vz = u
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
 
     P = getAirPressure(u, p, t)
     T = getAirTemp(u, p, t)
@@ -69,9 +83,8 @@ function getBalloonRadius(vBalloon)
 end
 
 function getProjectedArea(u, p, t)
-    z, vz = u
-    isBurst = p["isBurst"]
-    mGas = p["mGas"]
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    isBurst = p[:isBurst]
     if isBurst
         area = 2.0
     else
@@ -82,38 +95,44 @@ function getProjectedArea(u, p, t)
 end
 
 function force_buoyancy(u, p, t)
-    z, vz = u
-    mGas = p["mGas"]
-    isBurst = p["isBurst"]
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    isBurst = p[:isBurst]
 
     if isBurst
-        fBuoyant = 0.0
+        fBuoyant = [0.0; 0.0; 0.0]
     else
         V = getBalloonVolume(u, p, t)
         ρAir = getAirDensity(u, p, t)
-        fBuoyant = V*ρAir*g
+        fBuoyant = [0.0; 0.0; V*ρAir*g]
     end
     return fBuoyant
 end
 
 function force_drag(u, p, t)
-    z, vz = u
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
     ρAir = getAirDensity(u, p, t)
     A = getProjectedArea(u, p, t)
     Re = getReynoldsNumber(u, p, t)
     # If the balloon has burst, the drag coefficient is determined by the parachute (mostly)
-    if p["isBurst"]
-        Cd = 0.8
+    if p[:isBurst]
+        Cd = 0.7
     else
-        Cd = getDragCoeff(Re)
+        Cd = getBalloonDragCoeff(Re)
     end
 
-    fDrag = -(1/2)*ρAir*A*Cd*(vz^2)*sign(vz)
+    fDrag = [0.0; 0.0; -(1/2)*ρAir*A*Cd*(vz^2)*sign(vz)]
     return fDrag
 end
 
 function force_gravity(mBal, mGas, mPay, g)
-    return -g*(mBal + mGas + mPay)
+    return [0.0; 0.0; -g*(mBal + mGas + mPay)]
+end
+
+function getWindSpeed(u, p, t)
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    h = 10000.0
+    f = 3600.0
+    return [10.0, 0.0] #[sin(rz/h), cos(rz/h)] + 0.1*[sin(t/f), sin(t/f)]
 end
 
 function getReynoldsNumber(L, v, μ, ρ)
@@ -121,20 +140,19 @@ function getReynoldsNumber(L, v, μ, ρ)
 end
 
 function getReynoldsNumber(u, p, t)
-    z, vz = u
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
     V = getBalloonVolume(u, p, t)
     D = 2*getBalloonRadius(V)
     ρ, P, T, μ = getAirParams(u, p, t)
     return getReynoldsNumber(D, vz, μ, ρ)
 end
 
-function getDragCoeff(Re)
-    CdLaminar = 0.225
-    CdTurbulent = 0.425
+function getBalloonDragCoeff(Re)
+    CdLaminar = 0.425 
+    CdTurbulent = 0.225
     Re1 = 3.296e5
     ΔRe = 0.363e5
     Re2 = Re1 + ΔRe
-
     if Re < Re1
         Cd = CdLaminar
     elseif Re < Re2
@@ -149,7 +167,7 @@ end
 usingH2 = true
 
 # ISA atmospheric data at sea level.  This function uses geometric altitude (ordinary meters)
-ρ0, P0, T0, μ0 = ISAdata(0)
+ρ0, P0, T0, μ0 = ISAdata(0.0)
 
 pressureInitialAmbient = P0  # Pa - Air pressure at launch. For now, assume one standard atmosphere of pressure
 temperatureInitialAmbient = T0 # K - Air temperature at launch 15°C
@@ -180,26 +198,17 @@ mPay = 0.8 # kg - mass of the payload
 
 g = 9.81 # m/s^2
 
-function dFun(u, p, t)
-    z, vz = u
-    fDrag = force_drag(u, p, t)
-    fBuoyant = force_buoyancy(u, p, t)
-    fGrav = force_gravity(mBal, mGas, mPay, g)
-    az = (fDrag + fBuoyant + fGrav)/(mGas + mBal + mPay)
-    return [vz, az]
-end
-
 # Set up the callback that causes the balloon to burst when it becomes too large
 burstDiameter = 9.45 # m
 function cb_burst_condition(u, t, integrator)
-    isBurst = integrator.p["isBurst"]
+    isBurst = integrator.p[:isBurst]
     # Checking to see if callback has returned true before. If so, this prevents it from running again
     # https://discourse.julialang.org/t/differentialequations-callback-which-is-only-triggered-the-first-time-condition-is-satisfied/70216/3
     if isBurst
         return 2.0
     else
-        burstDiameter = integrator.p["burstDiameter"]
-        z, vZ = u
+        burstDiameter = integrator.p[:burstDiameter]
+        rx, ry, rz, vx, vy, vz, mGas, TGas = u
         V = getBalloonVolume(u, integrator.p, t)
         R = getBalloonRadius(V)
         balloonDiameter = 2*R
@@ -207,13 +216,19 @@ function cb_burst_condition(u, t, integrator)
     end
 end
 function cb_burst_affect!(integrator)
-    integrator.p["isBurst"] = true
+    # p is a namedtuple, which is immutable, so we convert to a dict, change one key,
+    # then convert back.  This is probably fine since it should only happen once during a run
+    p = Dict(pairs(integrator.p))
+    p[:isBurst] = true
+    p = NamedTuple(pairs(p))
+    integrator.p = p
+    return Nothing
 end
 
 # Callback to terminate the simulation at zero altitude
 function cb_negativeAltitude_condition(u, t, integrator)
-    z, vz = u
-    return z
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    return rz
 end 
 
 function cb_negativeAltitude_affect!(integrator)
@@ -223,16 +238,70 @@ end
 cb_burst = ContinuousCallback(cb_burst_condition, cb_burst_affect!)
 cb_negativeAltitude = ContinuousCallback(cb_negativeAltitude_condition, nothing, cb_negativeAltitude_affect!)
 cb_all = CallbackSet(cb_burst, cb_negativeAltitude)
-zInitial = 1.0
-zDotInitial = 0.0
-u0 = [zInitial, zDotInitial]
+
+# 37.762380, -122.418405 initial location NB
+# Initial position
+rx0 = 0.0
+ry0 = 0.0
+rz0 = 1.0 # Height some nonzero value so the simulation doesn't immediately terminate
+
+# Initial velocity
+vx0 = 0.0
+vy0 = 0.0
+vz0 = 0.0
+
+mGas0 = mGas
+TGas0 = temperatureInitialAmbient
+
+u0 = ComponentArray(rx=rx0, ry=ry0, rz=rz0, vx=vx0, vy=vy0, vz=vz0, mGas=mGas0, TGas=TGas0)
+
+# Initial and final times in seconds
+t0 = 0.0
+tFinal = 10000.0
 tSpan = [0.0, 10000.0]
-isBurst = false
 
-p = Dict("isBurst"=>isBurst, "mBal"=>mBal, "mPay"=>mPay, "mGas"=>mGas, "burstDiameter"=>burstDiameter)
+p = Dict(:isBurst=>false, :mBal=>mBal, :mPay=>mPay, :mGas=>mGas, :burstDiameter=>burstDiameter)
+p = NamedTuple([pair for pair in p])
 
+function dFun!(d, u, p, t)
+    rx, ry, rz, vx, vy, vz, mGas, TGas = u
+    ρt, Pt, Tt, μt = getAirParams(u, p, t) # air parameters for this specific timestep
+    uWind, vWind = getWindSpeed(u, p, t)
+    fDrag = force_drag(u, p, t)
+    fBuoyant = force_buoyancy(u, p, t)
+    fGrav = force_gravity(mBal, mGas, mPay, g)
+    ax, ay, az = (fDrag + fBuoyant + fGrav)/(mGas + mBal + mPay)
 
-prob = ODEProblem(dFun, u0, tSpan, p, callback=cb_all)
+    # dr/dt = v
+    d.rx = vx
+    d.ry = vy
+    d.rz = vz
+
+    # dv/dt = a
+    # Should closely follow external wind velocity
+    d.vx = (uWind - vx)^3
+    d.vy = (vWind - vy)^3
+    d.vz = az
+
+    # d/dt mGas
+    d.mGas = 0.0 # contant for now
+    # d/dt TGas
+    d.TGas = (Tt - TGas)^3 # Should stay tightly locked onto external temperature
+    return Nothing
+end
+# This will eventually be used when we have to upgrade to a proper DAE but for now we can fake it 
+#M = Diagonal([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+
+# Attach symbolic names 
+f! = ODEFunction(dFun!, syms=[Symbol(s) for s in labels(u0)])
+prob = ODEProblem(f!, u0, tSpan, p, callback=cb_all)
 sol = solve(prob)
 
-plot(sol)
+df = DataFrame(sol)
+df = rename(df, ("timestamp"=>"t"))
+simFolder = "./simulations/"
+simFile = "sol.csv"
+CSV.write(simFolder*simFile, df)
+#idxRz = label2index(u0, "rz")
+
+
---
+++ trajectoryView.jl
@@ -0,0 +1,107 @@
+using Statistics
+
+using CSV
+using DataFrames
+using Dates
+
+# Handling geotiff data
+using CoordinateTransformations
+using Rotations
+using StaticArrays
+using Geodesy
+using TiffImages
+using Rasters
+using ArchGDAL
+
+# 3d visualization
+using GLMakie
+using Mux
+
+#using Images
+#using FileIO
+
+# Defining the origin of the coordinate system for the trajectory
+lat = 37.7195
+lon = -122.4191
+alt = 81
+launchPoint = LLA(lat, lon, alt) # Field of Dogs, McLaren Park, SF
+# Define the transformation from local East, North, Up coordinates (in meters) to global Lat, Lon, Alt coordinates
+loc2glob = LLAfromENU(launchPoint, grs80) # we use grs80 because that's what the USGS uses
+glob2loc = inv(loc2glob)
+
+
+
+
+
+function importSimData(simFolder, simFile)
+    df = DataFrame(CSV.File(simFolder*simFile))
+
+    rx = df[!, :rx]
+    ry = df[!, :ry]
+    rz = df[!, :rz]
+    t = df[!, :t]
+    # Trajectory in global coordinates
+    rsGlobal = [loc2glob(ENU(rx[i], ry[i], rz[i])) for i in eachindex(rx)]
+    rLats = [r.lat for r in rsGlobal]
+    rLons = [r.lon for r in rsGlobal]
+    rAlts = [r.alt for r in rsGlobal]
+    return (t, rLats, rLons, rAlts)
+end
+
+function importHubData(hubFolder, hubFile)
+    df = DataFrame(CSV.File(hubFolder*hubFile))
+    rLats = df[!,:lat]
+    rLons = df[!,:lon]
+    rAlts = df[!,:alt]
+    datetimes = DateTime.(String.(df[!,:datetime]), dateformat"yyyy-mm-ddTHH:MM:SS.ssssssZ")
+    # extract the milliseconds since launch, then convert to seconds. We can't get the seconds
+    # directly because of some issue with rounding when converting
+    t = (Dates.value.(datetimes .- datetimes[1]))./1000.0
+    
+    return (t, rLats, rLons, rAlts)
+end
+
+# Import our test trajectory
+simFolder = "./simulations/"
+simFile = "sol.csv"
+simTime, simLats, simLons, simAlts = importSimData(simFolder, simFile)
+
+hubFolder = "./sondehub/"
+hubFile = "W1621037.csv" #"V4630041.csv"
+hubTime, hubLats, hubLons, hubAlts = importHubData(hubFolder, hubFile)
+
+geoDir = "geography/one arc sec/"
+tifPaths = readdir(geoDir)
+
+rasters = [Raster(geoDir*path) for path in tifPaths]
+rasters = [resample(rast, size=(500, 500)) for rast in rasters]
+
+geoBig = Rasters.mosaic(mean, rasters, atol=0.001)
+
+#=
+tifPathN38W123 = "geography/one arc sec/USGS_1_n38w123_20240313.tif"
+rasN38W123 = Raster(tifPathN38W123, lazy=false)
+rasN38W123 = resample(rasN38W123, size=(1000, 1000), method=:average)
+=#
+
+GLMakie.activate!
+set_theme!(backgroundcolor = :lightskyblue2)
+aspect=(1, 1, 1)
+perspectiveness=0.2
+fig= Figure(; size=(800, 800))
+ax1 = Axis3(fig[1, 1]; aspect, perspectiveness)
+
+xlims!(ax1, (-124, -119))
+ylims!(ax1, (36, 39))
+zlims!(ax1, (-10.0, 80000.0))
+#contourf!(ax, geoSmall, colormap=:batlow)
+surface!(ax1, geoBig, colormap=:oleron, nan_color=:red, colorrange=(-1499,1500))
+
+#surface!(ax1, rasN38W122, colormap=:winter, nan_color=:red, colorrange=(-1,1500))
+#surface!(ax1, rasN38W123, colormap=:winter, nan_color=:red, colorrange=(-1,1500))
+
+lines!(ax1, simLons, simLats, simAlts, color=simTime, linewidth=2, colormap=:winter)
+lines!(ax1, hubLons, hubLats, hubAlts, color=hubTime, linewidth=2, colormap=:lajolla)
+
+#cam = Makie.Camera3D(ax1.scene, projectiontype = Makie.Perspective, reset = Keyboard.left_control, center=false, reposition_button=Keyboard.left_alt)
+display(fig)
